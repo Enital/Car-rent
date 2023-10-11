ddf61f530440f15f311f60052d3d53814ad9e1cb
Enital <117684114+Enital@users.noreply.github.com>
2023-10-11 22:03:23 +0300

    fix centered position

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -37,65 +37,65 @@ export default function CatalogItem({ adverts, onFilter }) {
     onFilter(filter);
   };
   return (
-    <div className={css.container}>
-      <div className={css.filter}>
-        <form onSubmit={handleFormSubmit}>
-          <label htmlFor="Car brand">
-            <p className={css.formText}>Car brand</p>
-            <select
-              value={make}
-              onChange={e => setMake(e.target.value)}
-              className={css.select}
-            >
-              <option>Enter the text</option>
-              {makeArraySorted.map((make, index) => (
-                <option key={index} value={make}>
-                  {make}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label htmlFor="Price / 1 hour">
-            <p className={css.formText}>Price / 1 hour</p>
-            <select
-              value={price}
-              onChange={e => setPrice(e.target.value)}
-              className={css.select}
-            >
-              <option>To $</option>
-              {priceArraySorted.map((price, index) => (
-                <option key={index} value={price}>
-                  {price}
-                </option>
-              ))}
-            </select>
-          </label>
+    // <div className={css.container}>
+    <div className={css.filter}>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="Car brand">
+          <p className={css.formText}>Car brand</p>
+          <select
+            value={make}
+            onChange={e => setMake(e.target.value)}
+            className={css.select}
+          >
+            <option>Enter the text</option>
+            {makeArraySorted.map((make, index) => (
+              <option key={index} value={make}>
+                {make}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="Price / 1 hour">
+          <p className={css.formText}>Price / 1 hour</p>
+          <select
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+            className={css.select}
+          >
+            <option>To $</option>
+            {priceArraySorted.map((price, index) => (
+              <option key={index} value={price}>
+                {price}
+              </option>
+            ))}
+          </select>
+        </label>
 
-          <label htmlFor="Car mileage /km">
-            <p className={`${css.formText} ${css.formTextMile}`}>
-              Car mileage /km
-            </p>
-            <input
-              type="number"
-              value={minMileage}
-              onChange={e => setMinMileage(e.target.value)}
-              placeholder="From"
-              className={css.input}
-            />
-            <input
-              type="number"
-              value={maxMileage}
-              onChange={e => setMaxMileage(e.target.value)}
-              placeholder="To"
-              className={`${css.input} ${css.inputTo}`}
-            />
-          </label>
+        <label htmlFor="Car mileage /km">
+          <p className={`${css.formText} ${css.formTextMile}`}>
+            Car mileage /km
+          </p>
+          <input
+            type="number"
+            value={minMileage}
+            onChange={e => setMinMileage(e.target.value)}
+            placeholder="From"
+            className={css.input}
+          />
+          <input
+            type="number"
+            value={maxMileage}
+            onChange={e => setMaxMileage(e.target.value)}
+            placeholder="To"
+            className={`${css.input} ${css.inputTo}`}
+          />
+        </label>
 
-          <button type="submit" className={css.search}>
-            Search
-          </button>
-        </form>
-      </div>
+        <button type="submit" className={css.search}>
+          Search
+        </button>
+      </form>
     </div>
+    // </div>
   );
 }
